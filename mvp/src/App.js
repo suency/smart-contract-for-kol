71a099af067459ebae30fb98562165c52367ac6d
geng <ke.sun.ucl@gmail.com>
2023-05-21 15:35:56 +0800

    basic mvp test

---
+++ mvp/src/App.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+import 'antd/dist/reset.css';
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
