01c60e1f39942eb58f0c0c106147ebf2b2637816
geng <ke.sun.ucl@gmail.com>
2023-05-21 16:05:51 +0800

    add some query for contract

--- solidity/TransferContract.sol
+++ solidity/TransferContract.sol
@@ -10,41 +10,66 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract TransferContract {
     address public contractCreator;
-    uint256 public percentage; //seller get the percentage
-    uint256 public sellerBalance;
+    uint256 public percentage; //seller set the percentage
+    uint256 public sellerBalance; //seller balance in this contract
 
+    uint256 public contractBalance;
+    // wallet address => balance of this address in this contract
     struct WalletAddressValue {
         address walletAddress;
         uint256 balance;
     }
 
     constructor(uint256 initPercentage) {
-        contractCreator = msg.sender;
-        sellerBalance = 0;
-        percentage = initPercentage;
+        require(initPercentage >= 0 && initPercentage <=100 , "Amount should be >= 0 and <= 100");
+        contractCreator = msg.sender; // contract creator address = seller address
+        sellerBalance = 0; // seller balance in this contract
+        percentage = initPercentage; // the portion of tokens that seller get, value from (0-100)
+        contractBalance = 0;
     }
 
-     //key is the buyer and value is {address,100eth}
+     //key is the buyer address, value is map for {key:agent address, value is balance of tokens received}
     mapping(address => mapping(address => uint256)) public list;
 
     function transferToContract(address agentAddress) public payable {
         require(msg.value > 0, "Amount should be > 0");
 
+        // token allocations
         uint256 creatorAmount = (msg.value * percentage) / 100;
         uint256 agentAmount = (msg.value * (100 - percentage)) / 100;
-        sellerBalance += creatorAmount;
+        sellerBalance += creatorAmount; // update seller balance
 
-        payable(contractCreator).transfer(creatorAmount);
-        payable(agentAddress).transfer(agentAmount);
+        payable(contractCreator).transfer(creatorAmount); // pay to seller
+        payable(agentAddress).transfer(agentAmount); // pay to agent
 
-        list[msg.sender][agentAddress] += agentAmount;
+        list[msg.sender][agentAddress] += agentAmount; // update balance in list (buyer and agent)
     }
 
-    /* function getValue(address buyer, address agent) public view returns (uint256 Storage) {
-        return list[buyer][agent] ? list[buyer][agent] : 0;
+    // get data from list
+    function getList(address buyerAddress, address agentAddress) public view returns(uint256) {
+        return list[buyerAddress][agentAddress];
     }
-    
+
+    // get percentage from list
+    function getPercentage() public view returns(uint256) {
+        return percentage;
+    }
+
+    // get contract balance
+    function getContractBalance() public view returns(uint256) {
+        return contractBalance;
+    }
+
+    //contract creator can withdraw if there is contract balance
+    function withdrawBalance() public {
+        require(msg.sender == contractCreator, "Only the contract creator can withdraw");
+        uint256 balance = contractBalance;
+        contractBalance = 0;
+        payable(contractCreator).transfer(balance);
+    }
+
+    //contract can receive tokens
     receive() external payable {
-    
-    } */
+        contractBalance += msg.value;
+    }
 }
